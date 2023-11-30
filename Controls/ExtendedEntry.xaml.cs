279e1b4bc41459051a678bbbe9ee865f0c693fbe
JohnStavrou <johnstavrou@hotmail.com>
2023-11-30 13:35:06 +0200

    Init

---
+++ Controls/ExtendedEntry.xaml.cs
@@ -0,0 +1,170 @@
+#if ANDROID
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
+#elif WINDOWS
+
+#endif
+
+namespace ExtendedMauiControls.Controls;
+
+public partial class ExtendedEntry : ContentView
+{
+    public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(ExtendedEntry), "");
+    
+    private string placeholder;
+    private bool isPassword;
+    private bool hasClearButton = true;
+    private MaterialIcons? prefixIcon;
+    private MaterialIcons? suffixIcon;
+    private Color primaryColor;
+    private Color secondaryColor;
+
+    public ExtendedEntry()
+    {
+        InitializeHandlers();
+        InitializeComponent();
+    }
+
+    #region Handlers
+    private void InitializeHandlers()
+    {
+        // Official temporary workaround for url styled namespace in xaml
+        _ = new MauiIcon();
+
+        RemoveEntryUnderline();
+    }
+
+    private void RemoveEntryUnderline()
+    {
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoUnderline", (handler, view) =>
+        {
+            #if ANDROID
+            handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToAndroid());
+            #endif
+        });
+    }
+    #endregion
+
+    #region Public properties
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set
+        {
+            SetValue(TextProperty, value);
+            OnPropertyChanged(nameof(HasClearButton));
+        }
+    }
+
+    public string Placeholder
+    {
+        get => placeholder;
+        set
+        {
+            placeholder = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool IsPassword
+    {
+        get => isPassword;
+        set
+        {
+            isPassword = value;
+            if (value)
+                IsPasswordVisible = false;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool HasClearButton
+    {
+        get => hasClearButton && !string.IsNullOrEmpty(Text);
+        set
+        {
+            hasClearButton = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public MaterialIcons? PrefixIcon
+    {
+        get => prefixIcon;
+        set
+        {
+            prefixIcon = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public MaterialIcons? SuffixIcon
+    {
+        get => suffixIcon;
+        set
+        {
+            suffixIcon = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public Color PrimaryColor
+    {
+        get => primaryColor;
+        set => primaryColor = value;
+    }
+
+    public Color SecondaryColor
+    {
+        get => secondaryColor;
+        set => secondaryColor = value;
+    }
+    #endregion
+
+    public Color BorderColor => IsFocused ? PrimaryColor : SecondaryColor;
+
+    public MaterialIcons PasswordIcon => IsPasswordVisible ? MaterialIcons.Visibility : MaterialIcons.VisibilityOff;
+
+    private bool isFocused;
+    protected new bool IsFocused
+    {
+        get => isFocused;
+        set
+        {
+            isFocused = value;
+            OnPropertyChanged(nameof(BorderColor));
+        }
+    }
+
+    private bool isPasswordVisible;
+    protected bool IsPasswordVisible
+    {
+        get => isPasswordVisible;
+        set
+        {
+            isPasswordVisible = value;
+            entry.IsPassword = !isPasswordVisible;
+            OnPropertyChanged(nameof(PasswordIcon));
+        }
+    }
+
+    #region Events
+    [RelayCommand]
+    void InsetTapped() => entry.Focus();
+
+    [RelayCommand]
+    void PasswordVisibilityTapped() => IsPasswordVisible = !IsPasswordVisible;
+
+    [RelayCommand]
+    void ClearTapped()
+    {
+        entry.Text = "";
+        entry.Focus();
+    }
+
+    void Entry_Focused(object sender, FocusEventArgs e) => IsFocused = true;
+
+    void Entry_Unfocused(object sender, FocusEventArgs e) => IsFocused = false;
+
+    void Entry_TextChanged(object sender, TextChangedEventArgs e) => Text = e.NewTextValue;
+    #endregion
+}
---
+++ ExtendedMauiControls.cs
@@ -0,0 +1,15 @@
+﻿global using CommunityToolkit.Maui;
+global using CommunityToolkit.Mvvm.Input;
+global using MauiIcons.Core;
+global using MauiIcons.Material;
+
+namespace ExtendedMauiControls;
+
+public static class ExtendedMauiControls
+{
+    public static void UseExtendedMauiControls(this MauiAppBuilder builder)
+    {
+        builder.UseMauiCommunityToolkit();
+        builder.UseMaterialMauiIcons();
+    }
+}
